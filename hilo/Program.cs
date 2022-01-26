c132fb40f920afad0e882a2f1498cb34d19bab68
alexghow903 <97628131+alexghow903@users.noreply.github.com>
2022-01-26 07:36:04 -0700

    Add files via upload

---
+++ hilo/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace hilo 
+{
+    class Program
+    {
+        static int cards()
+        {
+            int card;
+            Random rand = new Random();
+            card = rand.Next(1, 14);
+            return card;
+        }
+
+        static int horl(int card, int ncard)
+        {
+            Console.Write("Higher of lower? [h/l] ");
+            string answer = Console.ReadLine();
+            if (answer == "h")
+            {
+                if (card < ncard)
+                {
+                    return 100;
+                }
+                else
+                {
+                    return -75;
+                }
+            }
+            else if (answer == "l")
+            {
+                if (card > ncard)
+                {
+                    return 100;
+                }
+                else
+                {
+                    return -75;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int points = 300;
+            int card = cards();
+            string answer;
+            while (points > 0)
+            {
+                int ncard = cards();
+                Console.WriteLine($"The card is: {card}");
+                points = points + horl(card, ncard);
+                Console.WriteLine($"Next card was: {ncard}");
+                Console.WriteLine($"Your score is: {points}");
+                card = ncard;
+                if (points > 0)
+                {
+                    Console.WriteLine("Play again? [y/n]");
+                    answer = Console.ReadLine();
+                    if (answer == "n")
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Game Over");
+        }
+    }
+}
