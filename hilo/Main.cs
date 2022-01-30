a7bad49dd57a689d9c8e2f10866668f1fa0388cb
alexghow903 <97628131+alexghow903@users.noreply.github.com>
2022-01-29 21:05:03 -0700

    Add files via upload

---
+++ hilo/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace hilo
+{
+    public class Main
+    {
+        public static void main(String[] args)
+        {
+            Program prog = new Program();
+            prog.control(); 
+        }       
+    }
+}
